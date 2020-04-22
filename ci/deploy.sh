fe789c7655598ba185d8dcd9f90b4c4afe699f15
Giancarlos Salas <giansalex@gmail.com>
2020-04-22 15:13:12 -0500

    Update url repository

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,6 +2,6 @@
 
 git config user.name "Giancarlos Salas";
 git config user.email "giansalex@gmail.com";
-git remote add gh-token "https://${GH_TOKEN}@github.com/thegreenter/thegreenter.github.io.git";
+git remote add gh-token "https://${GH_TOKEN}@github.com/thegreenter/docs.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 mkdocs gh-deploy -v --clean --remote-name gh-token --message "Deployed MkDocs";
