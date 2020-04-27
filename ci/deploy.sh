f8e62251733c0b139690053f9298dcb20f01f99f
Giancarlos Salas <giansalex@gmail.com>
2020-04-27 10:15:24 -0500

    Move deploy script to github-actions

--- ci/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git config user.name "Giancarlos Salas";
-git config user.email "giansalex@gmail.com";
-git remote add gh-token "https://${GH_TOKEN}@github.com/thegreenter/docs.git";
-git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
-mkdocs gh-deploy -v --clean --remote-name gh-token --message "Deployed MkDocs";
