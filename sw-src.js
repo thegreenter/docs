a57bdeddd6f2535b68ad845adeedd45166df4d03
Giancarlos Salas <giansalex@gmail.com>
2021-03-18 14:45:21 -0500

    Update workbox pwa

--- sw-src.js
+++ sw-src.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.2/workbox-sw.js');
 
 workbox.setConfig({debug: false});
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
