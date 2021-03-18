7b6d51de1059a8df463b7d035b11477934d25b24
Giancarlos Salas <giansalex@gmail.com>
2021-03-18 14:27:53 -0500

    Use google tag manager

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,3 +3,11 @@ if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js');
     });
 }
+
+location$.subscribe(function (url) {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  
+  gtag('config', 'UA-90097417-6');
+});
